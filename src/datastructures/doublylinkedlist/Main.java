4d27a9b5c63687c0b7294d84504753c38138e247
Tanishk Sahni <92090722+tanishksahni@users.noreply.github.com>
2023-08-04 11:10:55 +0530

    doubly linked list

---
+++ src/datastructures/doublylinkedlist/Main.java
@@ -0,0 +1,13 @@
+package datastructures.doublylinkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+        DoublyLinkedList myDLL = new DoublyLinkedList(7);
+        myDLL.append(9);
+        myDLL.getHead();
+        myDLL.getTail();
+        myDLL.getLength();
+        myDLL.printList();
+
+    }
+}
---
+++ src/datastructures/doublylinkedlist/Node.java
@@ -0,0 +1,10 @@
+package datastructures.doublylinkedlist;
+
+public class Node {
+    int value;
+    Node next;
+    Node prev;
+    Node(int value) {
+        this.value = value;
+    }
+}
