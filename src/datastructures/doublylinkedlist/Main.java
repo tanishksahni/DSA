6b5b4d711acccef1795babdee34982c45001f1b0
Tanishk Sahni <tanishksahni@Tanishks-MacBook-Air.local>
2023-08-23 11:15:54 +0530

    DLL methods

--- src/datastructures/doublylinkedlist/Main.java
+++ src/datastructures/doublylinkedlist/Main.java
@@ -4,10 +4,9 @@ public class Main {
     public static void main(String[] args) {
         DoublyLinkedList myDLL = new DoublyLinkedList(7);
         myDLL.append(9);
-        myDLL.getHead();
-        myDLL.getTail();
-        myDLL.getLength();
-        myDLL.printList();
+        System.out.println( myDLL.removeLast().value);
+        System.out.println( myDLL.removeLast().value);
+        System.out.println( myDLL.removeLast());
 
     }
 }
